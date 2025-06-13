6089cdcc4436b262daca20adb7b76aaa44439cb5
Vullwen <celian.pinquier@gmail.com>
2025-06-13 19:58:40 +0200

    modif yahou

--- relance.sh
+++ relance.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-if ! pgrep -f "./main" > /dev/null; then
-  sleep $((RANDOM % 271 + 30))
-  ./main &
-fi
+./main
+
+while true; do
+  if ! pgrep -f "./main" > /dev/null; then
+    sleep $((RANDOM % 271 + 30))
+    ./main &
+  fi
+  sleep 10  
+done
+
